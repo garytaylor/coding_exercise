5ebba2ded8311051f1765afae53e874a675da241
Gary Taylor <gary@wearefdm.com>
2013-05-02 16:52:40 +0100

    Tidied up

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,13 @@
 module ApplicationHelper
+  #Display distance in kilometres
   def miles_to_kilometres(miles,options={:round_to => 1})
     miles.nil? ? '' : (miles * 1.609344).round(options[:round_to])
   end
+  #Format a distance using any format (currently only km valid)
   def format_distance(miles, options={:format => :km, :round_to => 1})
     return "" if miles.nil?
     case options[:format]
       when :km then "#{miles_to_kilometres(miles,options)} km"
     end
-
-
   end
 end
--- app/models/property.rb
+++ app/models/property.rb
@@ -1,7 +1,10 @@
 class Property < ActiveRecord::Base
   attr_accessible :bedroom_count, :latitude, :longitude, :name, :rented_at, :image_path
+  #Scopes for valid and invalid
+  #Valid means not rented and invalid is the opposite
   scope :valid, where("rented_at IS NULL")
   scope :invalid, where("rented_at IS NOT NULL")
+
   #I am not claiming that I wrote the following :near scope.  It is an alternative to using a postgresql (which would probably be the preferred option).  The line of least resistance for this test really, but will still work well
   scope :near, lambda{ |options|
     origin = options[:origin]
@@ -26,13 +29,17 @@ class Property < ActiveRecord::Base
     }
   }
 
+
   class << self
+    #Converts degrees to radians for distance calculations
     def deg2rad(degrees)
       degrees * Math::PI / 180
     end
+    #Converts kilometres to miles
     def kilo_to_miles(kilo)
       kilo * 0.62137
     end
+    #Basic search
     def search(search_object)
       scope=self
       scope=scope.where("name ilike ?","%#{search_object[:name]}%") unless search_object[:name].nil? or search_object[:name].empty?
@@ -40,17 +47,19 @@ class Property < ActiveRecord::Base
       scope
     end
 
+    #Finds alternatives to any existing property (normally the invalid ones)
     def alternatives_to(invalid_results)
       scope=self.valid
       results=[]
       invalid_results.each do |invalid_result|
+        #Must have at least the same bedroom count if not more and must be within 20km of the original
         results.concat scope.where("bedroom_count>=?",invalid_result.bedroom_count).near(:origin => [invalid_result.latitude, invalid_result.longitude], :within => kilo_to_miles(20)).order("distance ASC").all
       end
       results
     end
   end
 
-  #As the distance ends up as a string, convert it here
+  #As the distance ends up as a string, convert it here.  @TODO Emulate a real attribute here
   def distance
     d=read_attribute(:distance)
     d.nil? ? nil : d.to_f
